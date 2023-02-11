590aa7253b71222cd1a95b011d5f01bcece8decd
Evgeny Novgorodov <e.novgorodov@tinkoff.ru>
2023-02-11 14:09:19 +0600

    Initial commit

---
+++ ToDoList/UI/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Evgeniy Novgorodov on 11.02.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+	// MARK: - Lifecycle
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .white
+	}
+}
