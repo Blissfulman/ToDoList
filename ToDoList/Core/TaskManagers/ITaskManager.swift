7ae8e8424b6302bb05d9bf8f4d77432d3fbedf62
Evgeny Novgorodov <e.novgorodov@tinkoff.ru>
2023-02-19 13:41:50 +0600

    Project structure was updated

--- ToDoList/Core/TaskManagers/ITaskManager.swift
+++ ToDoList/Core/TaskManagers/ITaskManager.swift
@@ -1,8 +1,8 @@
 //
-//  TaskManager.swift
+//  ITaskManager.swift
 //  ToDoList
 //
-//  Created by Evgeniy Novgorodov on 11.02.2023.
+//  Created by Evgeniy Novgorodov on 19.02.2023.
 //
 
 import Foundation
@@ -27,39 +27,4 @@ protocol ITaskManager: AnyObject {
 	/// - Returns: В случае успешного удаления задачи возвращается `true`, в противном случае — `false`.
 	@discardableResult
 	func deleteTask(_ task: Task) -> Bool
-}
-
-class TaskManager: ITaskManager {
-
-	// Properties
-	private var tasks = [Task]()
-
-	// MARK: - ITaskManager
-
-	var allTasks: [Task] {
-		tasks
-	}
-
-	var completedTasks: [Task] {
-		tasks.filter { $0.isCompleted }
-	}
-
-	var uncompletedTasks: [Task] { 
-		tasks.filter { !$0.isCompleted }
-	}
-
-	func addTask(_ task: Task) {
-		tasks.append(task)
-	}
-
-	func addTasks(_ tasks: [Task]) {
-		self.tasks = tasks
-	}
-
-	@discardableResult
-	func deleteTask(_ task: Task) -> Bool {
-		guard let index = tasks.firstIndex(where: { $0 === task }) else { return false }
-		tasks.remove(at: index)
-		return true
-	}
 }
